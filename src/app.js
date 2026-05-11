f33ee220f8460b026c78e31d4a9cd9132f5793cb
sachinkhandelwal003 <skhandelwal44@gmail.com>
2026-05-11 12:12:06 +0530

    code reffractor

--- src/app.js
+++ src/app.js
@@ -14,7 +14,18 @@ const app = express();
 
 // --- Middleware ---
 app.use(cors({
-  origin: ["http://localhost:3000", "http://localhost:3001", "http://localhost:3002", "http://localhost:3003", "http://localhost:3005"],
+  origin: [
+    "http://localhost:3000", 
+    "http://localhost:3001", 
+    "http://localhost:3002", 
+    "http://localhost:3003", 
+    "http://localhost:3005",
+    "http://127.0.0.1:3000",
+    "http://127.0.0.1:3001",
+    "http://127.0.0.1:3002",
+    "http://127.0.0.1:3003",
+    "http://127.0.0.1:3005"
+  ],
   credentials: true,
   methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"]
