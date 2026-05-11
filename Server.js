94686c72f5c6a093aa88ec3ef4d4df005fc8ca62
sachinkhandelwal003 <skhandelwal44@gmail.com>
2026-05-11 10:39:25 +0530

    fix: add join room event for real-time notifications

--- Server.js
+++ Server.js
@@ -23,6 +23,11 @@ const io = new Server(server);
 io.on("connection", (socket) => {
   console.log("A user connected:", socket.id);
 
+  socket.on("join", (userId) => {
+    socket.join(userId);
+    console.log(`User ${userId} joined their notification room: ${socket.id}`);
+  });
+
   socket.on("join_conversation", (conversationId) => {
     socket.join(conversationId);
     console.log(`User ${socket.id} joined conversation: ${conversationId}`);
