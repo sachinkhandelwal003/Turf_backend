aa6f4d1cfbf71d2a16f589684cfcffe91ff2ccb9
sachinkhandelwal003 <skhandelwal44@gmail.com>
2026-06-11 17:58:52 +0530

    new chnages

--- src/routes/notification.routes.js
+++ src/routes/notification.routes.js
@@ -5,7 +5,7 @@ import {
   markAllAsRead,
   deleteNotification
 } from '../controllers/notification.controller.js';
-import { protect } from '../middleware/auth.middleware.js';
+import { authMiddleware as protect } from '../middleware/auth.middleware.js';
 
 const router = express.Router();
 
