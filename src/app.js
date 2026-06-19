ff83e452e7f23cb699e069df0e9a61c99b735f64
sachinkhandelwal003 <skhandelwal44@gmail.com>
2026-06-19 16:51:33 +0530

    mnew changes

--- src/app.js
+++ src/app.js
@@ -55,7 +55,8 @@ app.use(cors({
   methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"]
 }));
-app.use(express.json());
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ limit: "50mb", extended: true }));
 app.use(passport.initialize());
 
 // --- Static Files ---
